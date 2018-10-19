c52758f604836558a13d97300bf6df6f4f5553d2
allan ferreira <allanferreira573@gmail.com>
2018-10-19 13:10:54 -0300

    todos arq

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,53 @@
+
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/jquery/ui/core.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/jquery/ui/widget.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/jquery/ui/mouse.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/jquery/ui/draggable.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/jquery/ui/datepicker.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/booked-appointments/assets/js/spin.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/booked-appointments/assets/js/spin.jquery.js ') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/booked-appointments/assets/js/tooltipster/js/jquery.tooltipster.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/booked-appointments/assets/js/functions.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/bbpress/templates/default/js/editor.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/comment-reply.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/responsive-poll/public/assets/js/vendors/chartjs_new.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/responsive-poll/public/assets/js/app/legend.js ') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/responsive-poll/public/assets/js/app/poll.js ') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/responsive-poll/public/assets/js/app/poll-init.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/themes/alliance/fw/js/responsive.init.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/themes/alliance/fw/js/core.reviews.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/themes/alliance/fw/js/core.utils.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/themes/alliance/fw/js/core.init.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/mediaelement/wp-mediaelement.min.js' ) }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/themes/alliance/fw/js/social/social-share.min.js' ) }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/wp-embed.min.js' ) }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/themes/alliance/fw/js/core.messages/core.messages.js' ) }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/themes/alliance/fw/shortcodes/shortcodes.js' ) }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/themes/alliance/fw/js/grid/grid.js' ) }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/themes/alliance/fw/js/jquery.flatshadow.js' ) }}"></script>
---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,104 @@
+
+
+<link rel='stylesheet'  href="{{ asset('wp-content/plugins/content_timeline/css/frontend/timeline.css') }}"  />
+
+<link rel='stylesheet' id='customScroll-css-css'  href="{{ asset('wp-content/plugins/content_timeline/css/frontend/jquery.mCustomScrollbar.css ') }}" type='text/css' media='all' />
+
+<link rel='stylesheet' id='prettyPhoto-css-css'  href="{{ asset('wp-content/plugins/content_timeline/css/frontend/prettyPhoto.css ') }}" type='text/css' media='all' />
+
+<link rel="stylesheet" id="my_timeline_font_awesome-css"  href="{{ asset('wp-content/plugins/content_timeline//css/frontend/awesome/font-awesome.css ') }}" type="text/css" media="all" />
+
+
+<link rel="stylesheet" id="my_timeline_lineicons_css-css"  href="{{ asset('wp-content/plugins/content_timeline//css/frontend/themify-icons.css  ') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="bbp-default-css"  href="{{ asset('wp-content/plugins/bbpress/templates/default/css/bbpress.css  ') }}" type="text/css" media="screen" />
+
+<link rel="stylesheet" id="booked-icons-css"  href="{{ asset('wp-content/plugins/booked-appointments/assets/icons.css ') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="booked-tooltipster-css"  href="{{ asset('wp-content/plugins/booked-appointments/assets/tooltipster/css/tooltipster.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="booked-tooltipster-theme-css"  href="{{ asset('wp-content/plugins/booked-appointments/assets/tooltipster/css/themes/tooltipster-light.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="booked-animations-css"  href="{{ asset('wp-content/plugins/booked-appointments/assets/animations.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="booked-styles-css"  href="{{ asset('wp-content/plugins/booked-appointments/assets/styles.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="booked-responsive-css"  href="{{ asset('wp-content/plugins/booked-appointments/assets/responsive.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="bp-legacy-css-css"  href="{{ asset('wp-content/plugins/buddypress/bp-templates/bp-legacy/css/buddypress.min.css') }}" type="text/css" media="screen" />
+
+<link rel="stylesheet" id="essential-grid-plugin-settings-css"  href="{{ asset('wp-content/plugins/essential-grid/public/assets/settings.css  ') }}" type="text/css" media="all" />
+
+
+<link rel="stylesheet" id="wpProQuiz_front_style-css"  href="{{ asset('wp-content/plugins/wp-pro-quiz/css/wpProQuiz_front.min.css ') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="weblator_polling-plugin-styles-css"  href="{{ asset('wp-content/plugins/responsive-poll/public/assets/public.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="weblator_polling-fa-css"  href="{{ asset('wp-content/plugins/responsive-poll/public/assets/font-awesome.min.css') }}" type="text/css" media="all" />
+
+
+<link rel="stylesheet" id="themerex-main-style-css"  href="{{ asset('wp-content/themes/alliance/style.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="themerex-shortcodes-style-css"  href="{{ asset('wp-content/themes/alliance/fw/shortcodes/shortcodes.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="themerex-animation-style-css"  href="{{ asset('wp-content/themes/alliance/fw/css/core.animation.min.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="bbpress-style-css"  href="{{ asset('wp-content/themes/alliance/css/bbpress-style.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="buddypress-style-css"  href="{{ asset('wp-content/themes/alliance/css/buddypress-style.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="themerex-skin-style-css"  href="{{ asset('wp-content/themes/alliance/skins/alliance/skin.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="themerex-custom-style-css"  href="{{ asset('wp-content/themes/alliance/fw/css/custom-style.min.css') }}" type="text/css" media="all" />
+
+
+<link rel="stylesheet" id="themerex-responsive-style-css"  href="{{ asset('wp-content/themes/alliance/css/responsive.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="mediaelement-css"  href="{{ asset('wp-includes/js/mediaelement/mediaelementplayer-legacy.min.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="wp-mediaelement-css"  href="{{ asset('wp-includes/js/mediaelement/wp-mediaelement.min.css') }}" type="text/css" media="all" />
+
+<link rel="stylesheet" id="themerex-messages-style-css"  href="{{ asset('wp-content/themes/alliance/fw/js/core.messages/core.messages.css') }}" type="text/css" media="all" />
+
+
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/jquery/jquery.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/jquery/jquery-migrate.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/content_timeline/js/frontend/jquery.easing.1.3.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/content_timeline/js/frontend/jquery.timeline.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/content_timeline/js/frontend/jquery.mousewheel.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/content_timeline/js/frontend/jquery.mCustomScrollbar.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/content_timeline/js/frontend/rollover.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/content_timeline/js/frontend/jquery.prettyPhoto.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/content_timeline//js/frontend/jquery.my_add_function.js') }}"></script>
+
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/buddypress/bp-core/js/confirm.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/buddypress/bp-core/js/widget-members.min.js ') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/buddypress/bp-core/js/jquery-query.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/buddypress/bp-core/js/vendor/jquery-cookie.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/buddypress/bp-core/js/vendor/jquery-scroll-to.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/buddypress/bp-templates/bp-legacy/js/buddypress.min.js ') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-content/plugins/responsive-poll/public/assets/js/vendors/excanvas.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/mediaelement/mediaelement-and-player.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('wp-includes/js/mediaelement/mediaelement-migrate.min.js') }}"></script>
+
+
+
+
